e241351c52bb589e3e04793d3b9520edf72aa021
Landon Labuskes <lplabuskes@gmail.com>
2023-12-03 17:04:28 -0800

    Day 3, part 1

---
+++ Day03/part1.cpp
@@ -0,0 +1,112 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+
+struct Point
+{
+    Point(int x, int y): x(x), y(y){};
+    bool operator==(const Point& p) const{return p.x==x && p.y==y;};
+    int x;
+    int y;
+};
+
+template<>
+struct std::hash<Point>
+{
+    std::size_t operator()(const Point& p) const
+    {
+        return std::hash<int>()(p.x) ^ std::hash<int>()(p.y);
+    }
+};
+
+struct Number
+{
+    int val = 0;
+    std::vector<Point> coordinates;
+};
+
+void add_symbol(int row, int col, std::unordered_set<Point>& adjacent_spaces)
+{
+    // Don't really care about bounds because we're just producing a set to check against
+    adjacent_spaces.insert(Point(row - 1, col - 1));
+    adjacent_spaces.insert(Point(row - 1, col));
+    adjacent_spaces.insert(Point(row - 1, col + 1));
+    adjacent_spaces.insert(Point(row, col - 1));
+    adjacent_spaces.insert(Point(row, col + 1));
+    adjacent_spaces.insert(Point(row + 1, col - 1));
+    adjacent_spaces.insert(Point(row + 1, col));
+    adjacent_spaces.insert(Point(row + 1, col + 1));
+}
+
+int main()
+{
+    std::ifstream file("input.txt");
+    std::vector<Number> numbers;
+    std::unordered_set<Point> adjacent_spaces;
+
+    int row = 0;
+    while(file)
+    {
+        std::string line;
+        std::getline(file, line);
+
+        bool reading_number = false;
+        Number current_number;
+        for (int col = 0; col < line.length(); col++)
+        {
+            if (line[col] == '.')
+            {
+                if (reading_number)
+                {
+                    numbers.push_back(current_number);
+                    current_number = Number();
+                    reading_number = false;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else if (line[col] >= '0' && line[col] <= '9')
+            {
+                reading_number = true;
+                current_number.val *= 10;
+                current_number.val += line[col] - '0';
+                current_number.coordinates.push_back(Point(row, col));
+            }
+            else  // i.e. this is a symbol
+            {
+                if (reading_number)
+                {
+                    numbers.push_back(current_number);
+                    current_number = Number();
+                    reading_number = false;
+                }
+                add_symbol(row, col, adjacent_spaces);
+            }
+        }
+        // Gotten to the end of the row. If we were reading a number, we need to add that
+        if (reading_number)
+        {
+            numbers.push_back(current_number);
+        }
+        row++;
+    }
+
+    int sum = 0;
+    for (Number n : numbers)
+    {
+        for (Point p : n.coordinates)
+        {
+            if (adjacent_spaces.find(p) != adjacent_spaces.end())
+            {
+                sum += n.val;
+                break;
+            }
+        }
+    }
+    std::cout << sum << std::endl;
+}
